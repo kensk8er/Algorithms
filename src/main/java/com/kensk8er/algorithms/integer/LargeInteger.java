1ccd9363f297e54fbbdfc7c5fd10075e5109ea39
kensk8er <kensk8er1017@gmail.com>
2014-05-30 00:13:42 +0100

    Added a comment.

--- src/main/java/com/kensk8er/algorithms/integer/LargeInteger.java
+++ src/main/java/com/kensk8er/algorithms/integer/LargeInteger.java
@@ -8,6 +8,16 @@ import java.util.regex.Pattern;
  * Created by kensk8er
  */
 public class LargeInteger {
+    /**
+     * LargeInteger class can be used to perform +/-/* operations for large integers that can't fit
+     * in int object in Java.
+     *
+     * E.g.
+     * LargeInteger.multiplyLargeIntegers("3141592653589793238462643383279502884197169399375105820974944592", "2718281828459045235360287471352662497757247093699959574966967627")
+     * -> 8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184
+     *
+     * Note that arguments need to be String objects.
+     */
 
     private static final String MINUS_SIGN = "-";
 
