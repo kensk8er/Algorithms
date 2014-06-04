8878f47c5673e2be21508a421b84b215c73b8ce0
kensk8er <kensk8er1017@gmail.com>
2014-06-05 00:56:36 +0100

    Added test cases.

--- src/test/java/com/kensk8er/algorithms/graph/MinCutTest.java
+++ src/test/java/com/kensk8er/algorithms/graph/MinCutTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MinCutTest {
     @Test
     void testFindMinCutDegree() {
-        Graph matrix = new Graph(decrementOne(Arrays.asList(
+        Graph matrix1 = new Graph(decrementOne(Arrays.asList(
                 Arrays.asList(2,3,4,7),
                 Arrays.asList(1,3,4),
                 Arrays.asList(1,2,4),
@@ -25,7 +25,99 @@ class MinCutTest {
                 Arrays.asList(1,5,6,8),
                 Arrays.asList(5,6,7)
         )));
-        assertEquals(findMinCutDegree(matrix), 2);
+        assertEquals(findMinCutDegree(matrix1), 2);
+
+        Graph matrix2 = new Graph(decrementOne(Arrays.asList(
+                Arrays.asList(4, 2, 7, 3),
+                Arrays.asList(4, 1, 3),
+                Arrays.asList(1, 2, 4),
+                Arrays.asList(5, 1, 2, 3),
+                Arrays.asList(8, 7, 6, 4),
+                Arrays.asList(8, 5, 7),
+                Arrays.asList(6, 8, 5, 1),
+                Arrays.asList(7, 6, 5)
+        )));
+        assertEquals(findMinCutDegree(matrix2), 2);
+
+        Graph matrix3 = new Graph(decrementOne(Arrays.asList(
+                Arrays.asList(2, 3, 4),
+                Arrays.asList(1, 3, 4),
+                Arrays.asList(1, 2, 4),
+                Arrays.asList(1, 2, 3, 5),
+                Arrays.asList(4, 6, 7, 8),
+                Arrays.asList(5, 7, 8),
+                Arrays.asList(5, 6, 8),
+                Arrays.asList(5, 6, 7)
+        )));
+        assertEquals(findMinCutDegree(matrix3), 1);
+
+        Graph matrix4 = new Graph(decrementOne(Arrays.asList(
+                Arrays.asList(1, 3, 4, 2),
+                Arrays.asList(2, 1, 4, 3),
+                Arrays.asList(3, 1, 2, 4),
+                Arrays.asList(4, 5, 3, 2, 1),
+                Arrays.asList(5, 4, 8, 6, 7),
+                Arrays.asList(6, 8, 7, 5),
+                Arrays.asList(7, 5, 8, 6),
+                Arrays.asList(8, 5, 7, 6)
+        )));
+        assertEquals(findMinCutDegree(matrix4), 1);
+
+        Graph matrix5 = new Graph(decrementOne(Arrays.asList(
+                Arrays.asList(1, 19, 15, 36, 23, 18, 39),
+                Arrays.asList(2, 36, 23, 4, 18, 26, 9),
+                Arrays.asList(3, 35, 6, 16, 11),
+                Arrays.asList(4, 23, 2, 18, 24),
+                Arrays.asList(5, 14, 8, 29, 21),
+                Arrays.asList(6, 34, 35, 3, 16),
+                Arrays.asList(7, 30, 33, 38, 28),
+                Arrays.asList(8, 12, 14, 5, 29, 31),
+                Arrays.asList(9, 39, 13, 20, 10, 17, 2),
+                Arrays.asList(10, 9, 20, 12, 14, 29),
+                Arrays.asList(11, 3, 16, 30, 33, 26),
+                Arrays.asList(12, 20, 10, 14, 8),
+                Arrays.asList(13, 24, 39, 9, 20),
+                Arrays.asList(14, 10, 12, 8, 5),
+                Arrays.asList(15, 26, 19, 1, 36),
+                Arrays.asList(16, 6, 3, 11, 30, 17, 35, 32),
+                Arrays.asList(17, 38, 28, 32, 40, 9, 16),
+                Arrays.asList(18, 2, 4, 24, 39, 1),
+                Arrays.asList(19, 27, 26, 15, 1),
+                Arrays.asList(20, 13, 9, 10, 12),
+                Arrays.asList(21, 5, 29, 25, 37),
+                Arrays.asList(22, 32, 40, 34, 35),
+                Arrays.asList(23, 1, 36, 2, 4),
+                Arrays.asList(24, 4, 18, 39, 13),
+                Arrays.asList(25, 29, 21, 37, 31),
+                Arrays.asList(26, 31, 27, 19, 15, 11, 2),
+                Arrays.asList(27, 37, 31, 26, 19, 29),
+                Arrays.asList(28, 7, 38, 17, 32),
+                Arrays.asList(29, 8, 5, 21, 25, 10, 27),
+                Arrays.asList(30, 16, 11, 33, 7, 37),
+                Arrays.asList(31, 25, 37, 27, 26, 8),
+                Arrays.asList(32, 28, 17, 40, 22, 16),
+                Arrays.asList(33, 11, 30, 7, 38),
+                Arrays.asList(34, 40, 22, 35, 6),
+                Arrays.asList(35, 22, 34, 6, 3, 16),
+                Arrays.asList(36, 15, 1, 23, 2),
+                Arrays.asList(37, 21, 25, 31, 27, 30),
+                Arrays.asList(38, 33, 7, 28, 17, 40),
+                Arrays.asList(39, 18, 24, 13, 9, 1),
+                Arrays.asList(40, 17, 32, 22, 34, 38)
+        )));
+        assertEquals(findMinCutDegree(matrix5), 3);
+
+        Graph matrix6 = new Graph(decrementOne(Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5),
+                Arrays.asList(2, 3, 4, 1),
+                Arrays.asList(3, 4, 1, 2),
+                Arrays.asList(4, 1, 2, 3, 8),
+                Arrays.asList(5, 1, 6, 7, 8),
+                Arrays.asList(6, 7, 8, 5),
+                Arrays.asList(7, 8, 5, 6),
+                Arrays.asList(8, 4, 6, 5, 7)
+        )));
+        assertEquals(findMinCutDegree(matrix6), 2);
     }
 
     private List<List<Integer>> decrementOne(List<List<Integer>> matrix) {
